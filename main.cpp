413d4c4428bdcaef4c92dddc4249dd7c3c7bfe34
Dan <>
2019-03-17 06:06:07 -0400

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Dan
+ *
+ * Created on March 17, 2019, 5:39 AM
+ */
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+/*Simple program demonstrates utilizing multiple functions.
+ *Asks user for dimensions of room to be cleaned, then calculates area
+ *and carpet cleaning cost based on flat fee of $99 for 10x12 + .25/sqft above
+ * 120sqft.
+ * The program does use type double for dollars and cents, which is not typically
+ * recommended, but it's only working with increments of 25 cents and the point 
+ * is really only to demo function use, so I went with it anyway.
+ * 
+ */
+
+int GetArea();
+double CleaningCost(int area);
+   
+  
+int main(int argc, char** argv) {
+    int roomArea;
+    
+    roomArea = GetArea();
+    cout << "Your total carpet cleaning cost for a " << roomArea << 
+            " sized room is $" <<fixed << setprecision(2) << CleaningCost(roomArea) << endl;
+    
+    return 0;
+}
+
+int GetArea() {
+    double roomLength;
+    double roomWidth;
+    
+    cout << "Enter the room length in feet" << endl;
+    cin >> roomLength;
+    cout << "Enter room width in feet" << endl;
+    cin >> roomWidth;
+    return static_cast<int> (ceil(roomLength * roomWidth));
+}
+
+double CleaningCost(int area) {
+    int flatCostArea = 120;
+    
+    if (area > flatCostArea) {
+        return 99.0 + (0.25 * (area - flatCostArea));
+    } else {
+        return flatCostArea;
+    }
+}
